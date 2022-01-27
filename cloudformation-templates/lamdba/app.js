67ba5be5e6891b6420acbceedddb870e5dec8aa8
Nathan Essel <nathanessel@gmail.com>
2022-01-27 10:27:05 -0500

    fixed bugs with api call

--- cloudformation-templates/lamdba/app.js
+++ cloudformation-templates/lamdba/app.js
@@ -25,7 +25,7 @@ app.use(function(req, res, next) {
   let interestRate = req.query.rate;
   let monthlyPayment = req.query.monthly;
   let loanLength = req.query.length;
-  let loan;
+  let loan = {};
 
   if (!amount || !interestRate || !monthlyPayment || !loanLength) {
     res.statusCode = 500;
@@ -39,10 +39,9 @@ app.use(function(req, res, next) {
 
   dynamodb.scan(params, (err, data) => {
     if (err) {
-      res.json({error: 'Error fetching loans from db'});
+      res.json({error: 'Error occurred while fetching loan from db'});
     }
     data.Items.forEach(item => {
-        console.log('item', item);
         if (item.amount == amount 
             && item.interestRate == interestRate 
             && item.monthlyPayment == monthlyPayment 
@@ -50,10 +49,7 @@ app.use(function(req, res, next) {
                 loan = item;
             }
     });
-    if (loan)
-      res.json({data: loan});
-    else
-      res.json({error: 'Could not find matching loan or an error has occurred'});
+    res.json({data: loan});
   });
 });
 
@@ -67,12 +63,6 @@ app.put(path, function(req, res) {
   let monthlyPayment = req.body.monthly;
   let loanLength = req.body.length;
   let loanId = req.body.loanId;
-
-  console.log('amount', amount);
-  console.log('interestRate', interestRate);
-  console.log('monthlyPayment', monthlyPayment);
-  console.log('loanLength', loanLength);
-  console.log('loanId', loanId);
 
   if (!amount || !interestRate || !monthlyPayment || !loanLength || !loanId) {
     res.statusCode = 500;
@@ -125,7 +115,6 @@ app.post(path, function(req, res) {
     if (err) {
       res.json({ error: 'Could not load items: ' + err.message });
     }
-      console.log('count', data);
       loanId = +(data.Count) + 1;
       let putItemParams = {
         TableName: tableName,
